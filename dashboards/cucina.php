aa96270c43dfa1b15801612ae80bdbb92a1cb761
potatoesaremashed <gabryboss2007@gmail.com>
2026-02-12 11:25:56 +0100

    fix dark mode + components

--- dashboards/cucina.php
+++ dashboards/cucina.php
@@ -22,6 +22,8 @@ include '../include/header.php';
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="../css/cucina.css">
+<link rel="stylesheet" href="../css/common.css">
+
 
 
 <div class="sticky-header">
--- dashboards/tavolo.php
+++ dashboards/tavolo.php
@@ -17,6 +17,8 @@ $prodotti = $conn->query("SELECT * FROM alimenti");
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link rel="stylesheet" href="../css/tavolo.css">
+<link rel="stylesheet" href="../css/common.css">
+
 
 
 <div class="container-fluid">
