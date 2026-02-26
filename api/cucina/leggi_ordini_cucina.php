2ee17b8f4e85d9aa9089edbfa24c34ef23da3135
yijin <yijingye89@gmail.com>
2026-02-26 23:10:35 +0100

    commenti leggi_ordini_cucina.php

--- api/cucina/leggi_ordini_cucina.php
+++ api/cucina/leggi_ordini_cucina.php
@@ -5,12 +5,12 @@ session_start();
 include "../../include/conn.php";
 header('Content-Type: application/json');
 
-if ($conn->connect_error) {
-    echo json_encode(["error" => "DB error: " . $conn->connect_error]);
+if ($conn->connect_error) { //se la connessione al database non fosse stabilita
+    echo json_encode(["error" => "DB error: " . $conn->connect_error]); //stampa json con error e messaggio di errore
     exit;
 }
 
-// Maxi Query SQL "Cucinetta" - Cerca tutti gli ordini che non sono stati marchiati "pronto"
+// Query SQL - Cerca tutti gli ordini che non sono stati marchiati "pronto"
 // Facendo LEFT JOIN in modo da pescare anche se c'è un commento ("note") a latere di una pietanza singola o solo nomiclatori
 $sql = "SELECT o.id_ordine, o.id_tavolo, t.nome_tavolo, o.stato, o.data_ora,
             d.quantita, a.nome_piatto, d.note
@@ -22,30 +22,30 @@ $sql = "SELECT o.id_ordine, o.id_tavolo, t.nome_tavolo, o.stato, o.data_ora,
         ORDER BY o.data_ora ASC";
 
 $res = $conn->query($sql);
-if (!$res) {
-    echo json_encode(["error" => $conn->error]);
+if (!$res) { //se la query non fosse eseguita
+    echo json_encode(["error" => $conn->error]); //stampa json con error e messaggio di errore
     exit;
 }
 
 $ordini = [];
 
 // Raggruppamento per numero Ticket Unico
-while ($row = $res->fetch_assoc()) {
-    $id = $row['id_ordine'];
-    if (!isset($ordini[$id])) {
+while ($row = $res->fetch_assoc()) { //finchè ci sono righe da fetchare
+    $id = $row['id_ordine']; //assegna l'id dell'ordine
+    if (!isset($ordini[$id])) { //se l'id non fosse presente
         // Scatola Generale
         $ordini[$id] = [
             'id_ordine' => $id,
-            'tavolo' => !empty($row['nome_tavolo']) ? $row['nome_tavolo'] : "Tavolo " . $row['id_tavolo'],
+            'tavolo' => !empty($row['nome_tavolo']) ? $row['nome_tavolo'] : "Tavolo " . $row['id_tavolo'], //se il nome del tavolo non fosse presente, assegna "Tavolo " + id_tavolo
             'stato' => $row['stato'],
-            'ora' => date('H:i', strtotime($row['data_ora'])),
+            'ora' => date('H:i', strtotime($row['data_ora'])), //converte la data e l'ora in formato H:i
             'piatti' => []
         ];
     }
 
     // Piatti inseriti dentro la Scatola Generale Ticket
-    $ordini[$id]['piatti'][] = ['nome' => $row['nome_piatto'], 'qta' => $row['quantita'], 'note' => $row['note'] ?? ''];
+    $ordini[$id]['piatti'][] = ['nome' => $row['nome_piatto'], 'qta' => $row['quantita'], 'note' => $row['note'] ?? '']; //se la nota non fosse presente, assegna ''
 }
 
-echo json_encode(array_values($ordini));
+echo json_encode(array_values($ordini)); //converte l'array associativa in un array numerico
 ?>
